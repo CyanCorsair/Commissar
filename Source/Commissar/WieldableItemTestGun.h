905a45ba1ab51ca13d317dcd044df1af15fffe38
CyanCorsair <ss@bakzie.com>
2016-07-31 19:44:35 +0200

    Added crouching, skill struct.

---
+++ Source/Commissar/WieldableItemTestGun.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CommissarWieldableItem.h"
+#include "WieldableItemTestGun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMISSAR_API AWieldableItemTestGun : public ACommissarWieldableItem
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
