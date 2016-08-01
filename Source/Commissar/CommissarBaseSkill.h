ee683e36d3ab1d3b57c0cdd950b7ec86bc150938
CyanCorsair <ss@bakzie.com>
2016-08-01 22:24:45 +0200

    Revised skills, added basic attribute accessor methods.

---
+++ Source/Commissar/CommissarBaseSkill.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Object.h"
+#include "CommissarBaseSkill.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMISSAR_API UCommissarBaseSkill : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/Commissar/CommissarMedicineSkill.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CommissarBaseSkill.h"
+#include "CommissarMedicineSkill.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMISSAR_API UCommissarMedicineSkill : public UCommissarBaseSkill
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
