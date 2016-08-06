eb0b9cdd839966fef40a9e61d7b239d38ae31f8c
CyanCorsair <ss@bakzie.com>
2016-08-06 23:23:56 +0200

    Added medkit item and medkit skill code.

---
+++ Source/Commissar/CommissarConsumableMedkitItem.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CommissarConsumableItem.h"
+#include "CommissarConsumableMedkitItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMISSAR_API ACommissarConsumableMedkitItem : public ACommissarConsumableItem
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
