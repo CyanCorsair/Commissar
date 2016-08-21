daa84a40e257c268db7c30b0984ae66884b0c46e
CyanCorsair <silest@live.com>
2016-08-21 20:43:26 +0200

    Added 76 Series class.

---
+++ Source/Commissar/CommissarCivilanDefenseRifleItem.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CommissarWieldableItem.h"
+#include "CommissarCivilanDefenseRifleItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMISSAR_API ACommissarCivilanDefenseRifleItem : public ACommissarWieldableItem
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
